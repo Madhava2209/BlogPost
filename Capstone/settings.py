55d62387aecbc13341dc68fcd357508cfdcec312
Madhava2209 <52972972+Madhava2209@users.noreply.github.com>
2020-05-17 19:30:03 +0530

    m

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '188-fbwk!$p55cqo%vg0v-pn#1)7s*@f8k1d0#1mo!m$l&0w*1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["blog--posts.herokuapp.com"]
 
