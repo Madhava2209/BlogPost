593e59e93f6fe36485b586a0e8c9670bf355e020
Madhava2209 <52972972+Madhava2209@users.noreply.github.com>
2020-05-17 19:20:42 +0530

    m

--- Blog/models.py
+++ Blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class BlogPost(models.Model):
-    title= models.CharField(max_length=100)
+    title= models.CharField(max_length=1000)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now=True)
     author = models.CharField(max_length=100)
